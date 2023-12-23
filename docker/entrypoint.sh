679c05dc4b7cefc9ff9a1b18bcd444c8898e4dd0
Vince94 <ruotolovincenzo94@gmail.com>
2023-12-22 21:16:43 -0600

    Update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -10,12 +10,12 @@ git config --global core.editor "code --wait"
 # Ubuntu info
 if [[ $USER == $UBUNTU_USER ]] && [[ $UID == $UBUNTU_UID ]]; then
 	echo "User credentials:
-    - USERNAME: $HOSTNAME: ($USER:$UID)
+    - USERNAME: $HOSTNAME ($USER:$UID)
     - PASSWORD: $UBUNTU_PSW
     "
 else
 	echo "User is not set correctly!"
-	if ![[ $USER == $UBUNTU_USER ]]; then
+	if ! [[ $USER == $UBUNTU_USER ]]; then
 		echo "Username mismatch: $USER is not $UBUNTU_USER"
 	else
 		echo "UID mismatch: $UID is not $UBUNTU_UID"
